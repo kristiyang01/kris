9fc4f88ebb0f31885fe64642b3d9652d6216bfc8
kristiyang01 <74201174+kristiyang01@users.noreply.github.com>
2022-05-10 04:45:36 +0300

    Add files via upload

--- Book.h
+++ Book.h
@@ -5,14 +5,20 @@
 
 class Book{
 private:
-    bool deleted = 0;
     char* author = nullptr;
     char* title = nullptr;
     char* fileName = nullptr;
     char* description = nullptr;
     short unsigned rating = 0;
     char* ISBN = nullptr;
+
+    void setString(char* destination, const char* source);
+    void setRating(short unsigned rating);
+
 public:
-    Book(char* author, char* title, char *fileName, char* description, short unsigned rating, char* ISBN);
+    
+    Book();
+    Book(const char* author, const char* title, const char *fileName, const char* description, short unsigned rating, const char* ISBN);
+    Book& operator=(const Book& other);
     ~Book();
 };
